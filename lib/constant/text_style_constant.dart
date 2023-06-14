956936892bd133043779c57bb83df821122b0e6b
chocolate-kuma <natsumi0814okada@gmail.com>
2023-06-14 15:42:12 +0900

    add color and textstyle constant

---
+++ lib/constant/text_style_constant.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:sashisuseso/constant/color_constant.dart';
+
+class TextStyleConstant {
+  TextStyleConstant._();
+
+  static const bold8 = TextStyle(
+      fontFamily: 'NotoSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 8,
+      color: ColorConstant.black0);
+  static const bold16 = TextStyle(
+      fontFamily: 'NotoSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 16,
+      color: ColorConstant.black0);
+  static const bold24 = TextStyle(
+      fontFamily: 'NotoSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 24,
+      color: ColorConstant.black0);
+  static const bold32 = TextStyle(
+      fontFamily: 'NotoSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 32,
+      color: ColorConstant.black0);
+  static const normal8 = TextStyle(
+      fontFamily: 'NotoSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 8,
+      color: ColorConstant.black0);
+  static const normal16 = TextStyle(
+      fontFamily: 'NotoSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 16,
+      color: ColorConstant.black0);
+  static const normal24 = TextStyle(
+      fontFamily: 'NotoSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 24,
+      color: ColorConstant.black0);
+  static const normal32 = TextStyle(
+      fontFamily: 'NotoSans',
+      fontWeight: FontWeight.normal,
+      fontSize: 32,
+      color: ColorConstant.black0);
+}
