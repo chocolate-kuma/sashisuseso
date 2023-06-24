f032fdd4bbf7fc6e5453c3fa6a8ad39093e24378
chocolate-kuma <natsumi0814okada@gmail.com>
2023-06-24 21:47:43 +0900

    change main

--- lib/pages/main/main_page.dart
+++ lib/pages/main/main_page.dart
@@ -1,12 +1,27 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:sashisuseso/constant/color_constant.dart';
+import 'package:sashisuseso/pages/main/compornent/audio_button.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SashiSuseSo'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                AudioButton(text: 'さ'),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
