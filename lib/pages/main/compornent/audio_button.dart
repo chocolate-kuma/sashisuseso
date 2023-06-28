ec1f14e0c8806defd00299db0f80d0facd23c220
chocolate-kuma <natsumi0814okada@gmail.com>
2023-06-28 14:13:43 +0900

    add counter

--- lib/pages/main/compornent/audio_button.dart
+++ lib/pages/main/compornent/audio_button.dart
@@ -1,22 +1,46 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:sashisuseso/constant/text_style_constant.dart';
 
-class AudioButton extends StatelessWidget {
+class AudioButton extends StatefulWidget {
   const AudioButton({super.key, required this.text, required this.audio});
 
   final String text;
   final String audio;
 
   @override
+  State<AudioButton> createState() => _AudioButtonState();
+}
+
+class _AudioButtonState extends State<AudioButton> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     final audioPlayer = AudioPlayer();
     return Flexible(
       flex: 10,
-      child: ElevatedButton(
-          onPressed: () {
-            audioPlayer.play(AssetSource(audio));
-          },
-          child: Text(text)),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                audioPlayer.play(AssetSource(widget.audio));
+                _incrementCounter();
+              },
+              child: Text(widget.text)),
+          Text(
+            '$_counter',
+            style: TextStyleConstant.bold24,
+          )
+        ],
+      ),
     );
   }
 }
